a9d9bb1ee3b40bf873871618f2169910c64de10a
wally2222 <82655614+wally2222@users.noreply.github.com>
2021-05-06 12:46:33 +0200

    Update Parent.js

--- Parent.js
+++ Parent.js
@@ -1,3 +1,28 @@
+import "./styles.css";
+//import Employee from './Employee'
+import Parent from './Parent'
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see
+         some magic happen!</h2>
+
+         {/*<Employee />*/}
+         <Parent name='Bob'/>
+         
+    </div>
+  );
+}
+
+//
+
+
+
+
+
 import React from 'react'
 import {Component} from 'react'
 import ChildComponent from './ChildComponent'
